fead86847553d498258feada03aa7296697c0992
sbx_user1051 <sbx_user1051@169.254.16.77>
2025-08-12 22:29:03 +0000

    Tue Aug 12 22:29:03 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,4 +3,12 @@ class SumSolution:
     
     def compute(self, x, y):
         
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise ValueError("Not an int")
+
+        if not (0 <= x <= 100) or not (0 <= y <= 100):
+            raise ValueError("Not between 0 and 100")
+        
+        return x+y
+
 
