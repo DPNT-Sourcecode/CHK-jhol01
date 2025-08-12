1f401dde5fe9c19369e81bb32007cd7f4f5dacf9
sbx_user1051 <sbx_user1051@169.254.16.77>
2025-08-12 22:42:00 +0000

    Tue Aug 12 22:42:00 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,16 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
+        if not isinstance(skus, str): 
+            return -1
+
         prices = {"A" : 50, "B" : 30, "C" : 20, "D" :15}
+        offers = {"A" : (3,130), "B" : (2, 45)}
+
+        counts = {}
+        for 
 
         
+
 
 
